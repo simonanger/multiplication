89f34e8bb58ef86e4d30a37dcbda4c1d13026c9c
Simon Anger <simonanger89@gmail.com>
2023-03-11 10:32:17 +0000

    created first test

---
+++ src/main/java/microservices/book/multiplication/challenge/ChallengeGeneratorServiceImpl.java
@@ -0,0 +1,21 @@
+package microservices.book.multiplication.challenge;
+import org.springframework.stereotype.Service;
+import java.util.Random;
+
+@Service
+public class ChallengeGeneratorServiceImpl implements ChallengeGeneratorService {
+    private final Random random;
+
+    ChallengeGeneratorServiceImpl() {
+        this.random = new Random();
+    }
+
+    protected ChallengeGeneratorServiceImpl(final Random random) {
+        this.random = random;
+    }
+
+    @Override
+    public Challenge randomChallenge() {
+        return null;
+    }
+}
