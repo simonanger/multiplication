58cf602f558958bd5cb91d99f5e166d0304bab7b
Simon Anger <simonanger89@gmail.com>
2023-03-20 20:56:44 +0000

    created and tested ChallengeAttemptController and finished chapter 3

--- src/main/java/microservices/book/multiplication/challenge/ChallengeAttemptDTO.java
+++ src/main/java/microservices/book/multiplication/challenge/ChallengeAttemptDTO.java
@@ -1,5 +1,9 @@
 package microservices.book.multiplication.challenge;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
 import lombok.Value;
 
 /**
@@ -8,7 +12,11 @@ import lombok.Value;
 
 @Value
 public class ChallengeAttemptDTO {
+
+    @Min(1) @Max(99)
     int factorA, factorB;
+    @NotBlank
     String userAlias;
+    @Positive(message = "How could you possibly get a negative result here? Try again.")
     int guess;
 }
