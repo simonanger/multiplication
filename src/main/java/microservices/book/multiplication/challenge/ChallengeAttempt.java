a8736a1c64f07b9e450f13a6bc5ff342e5431196
Simon Anger <simonanger89@gmail.com>
2023-04-03 21:08:23 +0100

    amended user and challengeAttempt to become entitites

--- src/main/java/microservices/book/multiplication/challenge/ChallengeAttempt.java
+++ src/main/java/microservices/book/multiplication/challenge/ChallengeAttempt.java
@@ -1,4 +1,5 @@
 package microservices.book.multiplication.challenge;
+import jakarta.persistence.*;
 import lombok.*;
 import microservices.book.multiplication.user.User;
 
@@ -6,12 +7,16 @@ import microservices.book.multiplication.user.User;
  * Identifies the attempt from a {@link User} to solve a challenge.
  */
 
-@Getter
-@ToString
-@EqualsAndHashCode
+@Entity
+@Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class ChallengeAttempt {
+    @Id
+    @GeneratedValue
     private Long id;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "USER_ID")
     private User user;
     private int factorA;
     private int factorB;
--- src/main/java/microservices/book/multiplication/user/User.java
+++ src/main/java/microservices/book/multiplication/user/User.java
@@ -1,15 +1,24 @@
 package microservices.book.multiplication.user;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.*;
 
 /**
  * Stores information to identify the user
  */
 
-@Getter
-@ToString
-@EqualsAndHashCode
+@Entity
+@Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class User {
+    @Id
+    @GeneratedValue
     private Long id;
     private String alias;
+
+    public User(final String userAlias) {
+        this(null, userAlias);
+    }
 }
